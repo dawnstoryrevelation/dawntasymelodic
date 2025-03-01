623bb5ce5db539ac9c1b1efbfdf301905e8a8ecd
dawnstoryrevelation <musicaljasperj@gmail.com>
2025-03-01 11:49:52 +1100

    Update auth.js

--- auth.js
+++ auth.js
@@ -53,3 +53,17 @@ export function handleSignIn() {
             alert("Sign in failed: " + error.message);
         });
 }
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
