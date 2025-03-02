fa4f10782397b0a3a0751d54d922d4db4f8a0947
dawnstoryrevelation <musicaljasperj@gmail.com>
2025-03-02 11:43:40 +1100

    Update vue.config.js with correct repository name

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
- 
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/dawntasymelodic/' : '/',
+  outputDir: 'dist',
+  assetsDir: 'assets',
+  devServer: {
+    historyApiFallback: true
+  }
+}
